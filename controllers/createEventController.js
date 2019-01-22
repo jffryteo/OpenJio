52297064b07483217e3a014c663de392cd30e020
jffryteo <42734040+jffryteo@users.noreply.github.com>
2019-01-22 16:53:22 +0800

    Add files via upload

--- controllers/createEventController.js
+++ controllers/createEventController.js
@@ -6,6 +6,33 @@ module.exports = {
     res.render('index.ejs');
 },
 
+  registerUser: (req,res) =>{
+
+      let first_name = req.body.first_name;
+      let last_name = req.body.last_name;
+      let gender = req.body.gender;
+      let email = req.body.email;
+      let location = req.body.location;
+      let phone_number = req.body.phone_number;
+      let rating = '2';//harcode test
+
+      //let uploadedFile = req.files.image;
+      //let image_name = uploadedFile.name;
+      //let fileExtension = uploadedFile.mimetype.split('/')[1];
+      //image_name = username + '.' + fileExtension;
+      let query = "INSERT INTO `users` (first_name, last_name, gender, email, location, phone_num) VALUES ('" +
+                            first_name + "', '" + last_name + "', '" + gender + "', '" + email + "', '" + location + "', '" + phone_number + "')";
+
+                            db.query(query, (err, result) => {
+                                                  if (err) {
+                                                      return res.status(500).send(err);
+                                                  }
+                                                  res.redirect('/');
+                                                });
+
+
+  },
+
 getLogin: (req, res) => {
   res.render('login.ejs');
 },
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@
 module.exports = function(app){
 
 
-  const {getHomePage,getLogin,getCreateFromVoucher,getCreateFromScratch,getJoin,getUserAcc} = require('../controllers/createEventController.js');
+  const {getHomePage,getLogin,getCreateFromVoucher,getCreateFromScratch,getJoin,getUserAcc,registerUser} = require('../controllers/createEventController.js');
 
 
 // routing
 app.get('/',getHomePage);
 
 app.get('/login',getLogin);
-
+app.post('/login',registerUser);
 app.get('/createFromVoucher',getCreateFromVoucher);
 app.get('/createFromScratch',getCreateFromScratch);
 app.get('/join',getJoin);
